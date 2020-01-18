aaea61e4bd68c71a7aa45558bb2ad77c6badfaff
jeffmatarrese <jeff.matarrese@gmail.com>
2020-01-18 09:15:41 -0500

    BarGraph Prefab
    New folder and prefab w/ script to move up and down

---
+++ Assets/ScoreBar.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreBar : MonoBehaviour
+{
+    [SerializeField] public float scoreInput = 5.0f; // just for testing - fakes input value from JS
+    [SerializeField] public float barScaler = 0.1f; // just for testing - scales down graph
+
+    private float posY;
+    private float posX;
+    private float posZ;
+    private float scaleX;
+    private float scaleY;
+    private float scaleZ;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // get existing scale & coordinates
+        float posY = this.transform.position.y;
+        float posX = this.transform.position.x;
+        float posZ = this.transform.position.z;
+        float scaleX = this.transform.localScale.x;
+        float scaleY = this.transform.localScale.y;
+        float scaleZ = this.transform.localScale.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localScale = new Vector3(scaleX, (scoreInput * barScaler), scaleZ);
+        transform.position = new Vector3(posX, ((scoreInput * barScaler)/ 2), posZ);
+        // if > 0 pink, else purple - jeff to do
+
+    }
+}
