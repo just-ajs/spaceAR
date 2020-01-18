549261ac1195ab5d41543fa7c6022c5d9a9bf6e6
jeffmatarrese <jeff.matarrese@gmail.com>
2020-01-18 16:24:51 -0500

    NonuniformScalePrefab package added to project

---
+++ Assets/NonUniformScale/NonUniformScale.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NonUniformScale : MonoBehaviour
+{
+    [SerializeField]
+    private Transform transformToManipulate;
+
+    [SerializeField]
+    private Transform xHandle;
+
+    [SerializeField]
+    private Transform zHandle;
+
+    private Vector3 initialScale;
+
+    private const float defaultHandleScale = 0.1f;
+
+    private void Start()
+    {
+        initialScale = this.transformToManipulate.localScale;
+        this.transform.localScale = initialScale;
+
+        SetHandleScale(xHandle);
+        SetHandleScale(zHandle);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = transformToManipulate.position;
+        this.transformToManipulate.localScale = new Vector3(initialScale.x * this.xHandle.localPosition.x, initialScale.y, initialScale.z * this.zHandle.localPosition.z);
+    }
+
+    private void SetHandleScale(Transform input)
+    {
+        input.parent = null;
+        input.localScale = Vector3.one * defaultHandleScale;
+        input.parent = this.transform;
+    }
+}
---
+++ Assets/NonUniformScale/NonUniformScaleHandle.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.WSA.Input;
+
+public class NonUniformScaleHandle : MonoBehaviour
+{
+    private Vector3 initialPosition;
+
+    private enum Axis
+    {
+        x = 0,
+        z
+    }
+
+    private Axis currentAxis;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialPosition = this.transform.localPosition;
+
+        currentAxis = Mathf.Abs(initialPosition.x) > Mathf.Abs(initialPosition.z) ? Axis.x : Axis.z;
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (currentAxis == Axis.x)
+        {
+            this.transform.localPosition = new Vector3(this.transform.localPosition.x, initialPosition.y, initialPosition.z);
+        }
+        else
+        {
+            this.transform.localPosition = new Vector3(initialPosition.x, initialPosition.y, this.transform.localPosition.z);
+        }
+    }
+}
