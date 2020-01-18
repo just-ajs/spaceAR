a13119d5b06228e55231f3c0e39b023f4d436b03
lukegehron <luke@gehron.com>
2020-01-18 11:46:15 -0500

    pushChanges

---
+++ Assets/onReleaseZ.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class onReleaseZ : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    //public GameObject myCube;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, 0.0f, transform.position.z);
+
+    }
+}
