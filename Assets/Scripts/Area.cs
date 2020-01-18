2335dd2b064795d1766d0c676dad44c822b06f4c
Justyna Szychowska <ucqbjsz@ucl.ac.uk>
2020-01-18 11:43:54 -0500

    calculate area with number of people

--- Assets/Scripts/Area.cs
+++ Assets/Scripts/Area.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -10,8 +11,10 @@ public class Area : MonoBehaviour
 
     float lukeScale = 1f;
 
-    public float SquareFoorArea = 0f;
+    public float SquareFootArea = 0f;
     public float SquareMetersArea = 0f;
+
+    public int hackers = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -25,23 +28,34 @@ public class Area : MonoBehaviour
         width = this.transform.localScale.x;
         depth = this.transform.localScale.z;
 
-        SquareFoorArea = GetSquareFoot();
-        SquareMetersArea = FootToMeters(SquareFoorArea);
+        SquareFootArea = GetSquareFoot();
+        SquareMetersArea = FootToMeters(SquareFootArea);
+
+        hackers = GetHackers(SquareFootArea);
     }
 
     float GetSquareFoot()
-    { 
+    {
         float sq = width * depth;
 
 
         var scaling = 40000 / lukeScale;
 
         sq = sq * scaling;
-        return sq;
+        return Math.Abs(sq);
     }
 
     float FootToMeters(float foot)
     {
-        return foot * 0.092903f;
+        return (float)Math.Round(foot * 0.092903f);
+    }
+
+    int GetHackers(float foot)
+    {
+        int ppl = 0;
+
+        ppl = (int)Math.Floor(foot * 0.05);
+
+        return ppl;
     }
 }
