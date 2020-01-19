de48bb5f981a1bc43fa8e56a9af6066c19ce28ed
jeffmatarrese <jeff.matarrese@gmail.com>
2020-01-19 09:21:05 -0500

    button fixed

--- Assets/Scripts/RoomsInBoundingBox.cs
+++ Assets/Scripts/RoomsInBoundingBox.cs
@@ -26,7 +26,7 @@ public class RoomsInBoundingBox : MonoBehaviour
     // ui
     public GameObject feedbackSlider;
 
-    int[] array2 = new int[] { 1, 3, 5, 7, 9 }
+    int[] array2 = new int[] { 1, 3, 5, 7, 9 };
     //public string[] scoreBarText = new string[] {"Hackers\nx\nMentors", "} >;
 
 
