4b7469da2a5dea9a400e3f9c1470e759e2bf9f18
lukegehron <luke@gehron.com>
2020-01-18 23:51:10 -0500

    added functionality

---
+++ Assets/ifNotOnGrid.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ifNotOnGrid : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject scalerObject;
+    public GameObject toolTip;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameObject.transform.position.x < 0.3 && gameObject.transform.position.x > -0.3 && gameObject.transform.position.z < 1.2 && gameObject.transform.position.z > 0.6)
+        {
+            //ganeobject is on the grid
+            //check to see if the scalar script is on, and if it is, turn it off
+            //if (scalerObject.activeSelf == true) {
+                scalerObject.SetActive(false);
+                toolTip.SetActive(true);
+           // }
+            //turn off the scaler script
+        }
+        else {
+            //turn on the scaler gameobject
+            //if (scalerObject.activeSelf == false) {
+                scalerObject.SetActive(true);
+                toolTip.SetActive(false);
+            //}
+            //check to see if the scalar script is off, and if it is, turn it on
+        }
+    }
+}
---
+++ Assets/movePivotAboveAnchor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movePivotAboveAnchor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject anchor;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (gameObject.transform.position.x != anchor.transform.position.x || gameObject.transform.position.z != anchor.transform.position.z) {
+            gameObject.transform.position = new Vector3(anchor.transform.position.x, 0.1f, anchor.transform.position.z);
+        }
+    }
+}
