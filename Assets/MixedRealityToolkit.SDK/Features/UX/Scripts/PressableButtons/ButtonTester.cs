da509d4873bec944f9b03428244d83b0abe75a1e
jeffmatarrese <jeff.matarrese@gmail.com>
2020-01-18 20:21:40 -0500

    buttons bugs fixed

---
+++ Assets/MixedRealityToolkit.SDK/Features/UX/Scripts/PressableButtons/ButtonTester.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonTester : MonoBehaviour
+{
+    public void ButtonClickTest()
+    {
+        Debug.Log("I was clicked, my name is " + this.gameObject.name);
+    }
+}
